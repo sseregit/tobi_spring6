6d36154f84c4b1dd7ed03c95da90b93314dba0c3
sseregit <sseregit@naver.com>
2024-10-23 05:54:56 +0900

    DI와 디자인 패턴 (2)

--- src/main/java/toby/spring/hellospring/Client.java
+++ src/main/java/toby/spring/hellospring/Client.java
@@ -5,14 +5,24 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println("payment = " + payment);
+        System.out.println("=============================");
+        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("payment2 = " + payment2);
+        System.out.println("=============================");
+
+        TimeUnit.SECONDS.sleep(3);
+
+        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("payment3 = " + payment3);
     }
 }
--- src/main/java/toby/spring/hellospring/ObjectFactory.java
+++ src/main/java/toby/spring/hellospring/ObjectFactory.java
@@ -1,10 +1,22 @@
 package toby.spring.hellospring;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class ObjectFactory {
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(cachedExRateProvider());
+    }
 
+    @Bean
+    public ExRateProvider cachedExRateProvider() {
+        return new CachedExRateProvider(exRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
 }
