d18d924da8fd3368d369689a41690d477986823e
장연식 <sseregit@knowwheresoft.com>
2024-10-22 14:15:16 +0900

    PaymentService 개발 (3)

---
+++ src/main/java/toby/spring/hellospring/ExRateData.java
@@ -0,0 +1,13 @@
+package toby.spring.hellospring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(
+        String result,
+        Map<String, BigDecimal> rates
+) {
+}
