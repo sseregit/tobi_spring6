88b15c0f42479cf50ffb3b9e50a4f697c6e098da
장연식 <sseregit@knowwheresoft.com>
2024-10-22 15:36:39 +0900

    클래스의 분리

---
+++ src/main/java/toby/spring/hellospring/Client.java
@@ -0,0 +1,14 @@
+package toby.spring.hellospring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+//        PaymentService paymentService = new WebApiExRatePaymentService();
+        PaymentService paymentService = new SimpleExRatePaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("payment = " + payment);
+    }
+}
