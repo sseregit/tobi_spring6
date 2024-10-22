a0a37dbd8ce54a0c85b555b4dd7f1e04f977057c
sseregit <sseregit@naver.com>
2024-10-23 04:53:36 +0900

    구성정보를 가져오는 다른 방법

--- src/main/java/toby/spring/hellospring/ObjectFactory.java
+++ src/main/java/toby/spring/hellospring/ObjectFactory.java
@@ -1,17 +1,10 @@
 package toby.spring.hellospring;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-    @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
-    }
 
-    @Bean
-    public ExRateProvider exRateProvider() {
-        return new SimpleExRateProvider();
-    }
 }
--- src/main/java/toby/spring/hellospring/PaymentService.java
+++ src/main/java/toby/spring/hellospring/PaymentService.java
@@ -1,13 +1,15 @@
 package toby.spring.hellospring;
 
+import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Component
 public class PaymentService {
 
     private final ExRateProvider exRateProvider;
-
 
     public PaymentService(ExRateProvider exRateProvider) {
         this.exRateProvider = exRateProvider;
