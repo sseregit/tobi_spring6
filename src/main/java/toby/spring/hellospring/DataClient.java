0e8362a59e19250a9bacc8be81a92134358ed6cf
장연식 <sseregit@knowwheresoft.com>
2024-10-24 13:39:57 +0900

    Order 리포지토리와 예외

--- src/main/java/toby/spring/hellospring/DataClient.java
+++ src/main/java/toby/spring/hellospring/DataClient.java
@@ -1,9 +1,8 @@
 package toby.spring.hellospring;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import toby.spring.hellospring.data.OrderRepository;
 import toby.spring.hellospring.order.Order;
 
 import java.math.BigDecimal;
@@ -12,21 +11,14 @@ public class DataClient {
 
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
+        OrderRepository orderRepository = beanFactory.getBean(OrderRepository.class);
 
-        // em
-        EntityManager em = emf.createEntityManager();
-        // transaction
-        em.getTransaction().begin();
-
-        // em.persist
         Order order = new Order("100", BigDecimal.TEN);
-        em.persist(order);
-        System.out.println("order = " + order);
 
-        em.getTransaction().commit();
+        orderRepository.save(order);
 
-        em.close();
-        emf.close();
+        Order order2 = new Order("100", BigDecimal.ONE);
+
+        orderRepository.save(order2);
     }
 }
--- src/main/java/toby/spring/hellospring/DataConfig.java
+++ src/main/java/toby/spring/hellospring/DataConfig.java
@@ -1,5 +1,6 @@
 package toby.spring.hellospring;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -7,6 +8,7 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import toby.spring.hellospring.data.OrderRepository;
 
 import javax.sql.DataSource;
 
@@ -30,5 +32,10 @@ class DataConfig {
         }});
 
         return emf;
+    }
+
+    @Bean
+    OrderRepository orderRepository(EntityManagerFactory emf) {
+        return new OrderRepository(emf);
     }
 }
